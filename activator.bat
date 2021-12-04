0fc4a016771b9fa0c4f99d611712dcf6adfa7890
Guilherme <maxguilham@protonmail.com>
2021-12-04 18:07:51 -0300

    Add files via upload

---
+++ activator.bat
@@ -0,0 +1,7 @@
+@echo off
+title Activate Windows
+echo Starting script...
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms8.msguides.com
+slmgr /ato
+shutdown -r /f /t 0
---
+++ readme.txt
@@ -0,0 +1,3 @@
+PLEASE READ THIS! Use this if your machine isn`t with key! RUN AS ADMINISTRATOR THE SCRIPT!!!
+
+Creator: iiMaxxi/ItzMaxxi
